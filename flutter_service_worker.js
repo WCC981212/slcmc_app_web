4dd2a0e67305774ecaf3a3601767055bbda67083
WCC981212 <WCC981212@users.noreply.github.com>
2022-05-15 16:26:53 +0000

    AUTO deploy

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -7,7 +7,7 @@ const RESOURCES = {
 "assets/assets/logo_circle.png": "e6a890bf32f798c26dcdf0a08aba074e",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/fonts/MaterialIcons-Regular.otf": "95db9098c58fd6db106f1116bae85a0b",
-"assets/NOTICES": "4954a40a1f2c6aa47c332baa6862fd2f",
+"assets/NOTICES": "19c8f65f9a3327fb64dbff6ce8621774",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
 "canvaskit/canvaskit.js": "c2b4e5f3d7a3d82aed024e7249a78487",
 "canvaskit/canvaskit.wasm": "4b83d89d9fecbea8ca46f2f760c5a9ba",
@@ -18,9 +18,9 @@ const RESOURCES = {
 "flutter.js": "0816e65a103ba8ba51b174eeeeb2cb67",
 "icons/android-chrome-192x192.png": "300df5880ab089fee82947e13cc1f723",
 "icons/android-chrome-512x512.png": "e81b9a49fc75734c8de536ac2cdfbbf9",
-"index.html": "783d2742b647898b2074470fb460175b",
-"/": "783d2742b647898b2074470fb460175b",
-"main.dart.js": "6ae13fc13069a904ce0e3830995f1e93",
+"index.html": "353c6b9f735a11390a073a2cd7e0f992",
+"/": "353c6b9f735a11390a073a2cd7e0f992",
+"main.dart.js": "e2d706d97ab69a8d601138ce1c13f95d",
 "manifest.json": "86cf7f6ea23997dbbad7a61e0191d523",
 "version.json": "0a1fa595e5db892b1fb2e64c9afae037"
 };
