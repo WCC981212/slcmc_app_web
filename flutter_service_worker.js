fe6d5f51e256230bc331c916e8324b3575a5bca9
WCC981212 <WCC981212@users.noreply.github.com>
2022-05-16 14:58:15 +0000

    AUTO deploy

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -18,9 +18,9 @@ const RESOURCES = {
 "flutter.js": "0816e65a103ba8ba51b174eeeeb2cb67",
 "icons/android-chrome-192x192.png": "300df5880ab089fee82947e13cc1f723",
 "icons/android-chrome-512x512.png": "e81b9a49fc75734c8de536ac2cdfbbf9",
-"index.html": "f0b93aa1c05ddc8e71f7847046a991ad",
-"/": "f0b93aa1c05ddc8e71f7847046a991ad",
-"main.dart.js": "b73eb3559602fea8a0376f7ed0c1193e",
+"index.html": "bb2f11108995b8aa8277edf287cf045b",
+"/": "bb2f11108995b8aa8277edf287cf045b",
+"main.dart.js": "36fbd337607c5383b26da0154d8bf6e7",
 "manifest.json": "86cf7f6ea23997dbbad7a61e0191d523",
 "version.json": "0a1fa595e5db892b1fb2e64c9afae037"
 };
